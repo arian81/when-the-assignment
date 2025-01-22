ee385d64e94863b0a0a2e878d9baba68a0ba25e0
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2025-01-21 20:15:11 -0500

    feat: create basic MVP

---
+++ src/server/api/routers/assignment.ts
@@ -0,0 +1,66 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const assignmentRouter = createTRPCRouter({
+  get: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.db.assignment.findMany({
+      include: {
+        course: true,
+      },
+      orderBy: {
+        dueDate: "asc",
+      },
+    });
+  }),
+  create: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        url: z.string().optional(),
+        dueDate: z.date().refine((date) => date > new Date(), {
+          message: "Due date must be in the future",
+        }),
+        courseCode: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.assignment.create({
+        data: {
+          title: input.title,
+          url: input.url,
+          dueDate: input.dueDate,
+          courseCode: input.courseCode,
+        },
+        include: {
+          course: true,
+        },
+      });
+    }),
+  delete: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.assignment.delete({
+        where: { id: input.id },
+      });
+    }),
+});
+
+export const courseRouter = createTRPCRouter({
+  get: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.db.course.findMany({
+      orderBy: {
+        code: "asc",
+      },
+    });
+  }),
+  create: publicProcedure
+    .input(z.object({ code: z.string(), name: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.course.create({
+        data: {
+          code: input.code,
+          name: input.name,
+        },
+      });
+    }),
+});
